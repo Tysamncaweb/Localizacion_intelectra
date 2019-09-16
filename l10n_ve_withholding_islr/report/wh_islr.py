24e70870ab90b2f4998a8573c42260c1fd3a80c5
MFLORES2018 <vasquezmiguelfa@gmail.com>
2019-09-16 16:42:32 -0400

    Ajustes a los reportes de comprobante de retención de IVA/ISLR

--- l10n_ve_withholding_islr/report/wh_islr.py
+++ l10n_ve_withholding_islr/report/wh_islr.py
@@ -55,8 +55,10 @@ class RepComprobanteIslr(models.AbstractModel):
         return date_invoice
 
     def _get_supplier_invoice_number(self, id):
-
-        supplier_number = id[0].invoice_id.number
+        if 'out' in id[0].invoice_id.type:
+            supplier_number = id[0].invoice_id.number
+        else:
+            supplier_number = id[0].invoice_id.supplier_invoice_number
         return supplier_number
 
     def _get_nro_ctrl(self, id):
@@ -103,16 +105,22 @@ class RepComprobanteIslr(models.AbstractModel):
         split_date = date.split('-')
         return str(split_date[2]) + '/' + (split_date[1]) + '/' + str(split_date[0])
 
-
-    def get_direction(self, partner):
-        direction = ''
-        direction = ((partner.street and partner.street + ', ') or '') + \
-                    ((partner.street2 and partner.street2 + ', ') or '') + \
-                    ((partner.city and partner.city + ', ') or '') + \
-                    ((partner.state_id.name and partner.state_id.name + ',')or '')+ \
-                    ((partner.country_id.name and partner.country_id.name + '') or '')
-        if direction == '':
-            direction = 'Sin direccion'
+    #DATOS DEL AGENTE DE RETENCIÓN
+    #Direccion del agente retenido
+    def get_direction(self, value):
+        direction = 'Sin direccion'
+        if 'out' in value.type:
+            direction = ((value.partner_id.street and value.partner_id.street + ', ') or '') + \
+                    ((value.partner_id.street2 and value.partner_id.street2 + ', ') or '') + \
+                    ((value.partner_id.city and value.partner_id.city + ', ') or '') + \
+                    ((value.partner_id.state_id.name and value.partner_id.state_id.name + ',')or '')+ \
+                    ((value.partner_id.country_id.name and value.partner_id.country_id.name + '') or '')
+        else:
+            direction = ((value.company_id.partner_id.street and value.company_id.partner_id.street + ', ') or '') + \
+                        ((value.company_id.partner_id.street2 and value.company_id.partner_id.street2 + ', ') or '') + \
+                        ((value.company_id.partner_id.city and value.company_id.partner_id.city + ', ') or '') + \
+                        ((value.company_id.partner_id.state_id.name and value.company_id.partner_id.state_id.name + ',') or '') + \
+                        ((value.company_id.partner_id.country_id.name and value.company_id.partner_id.country_id.name + '') or '')
         return direction
 
     def get_tipo_doc(self, tipo=None):
@@ -121,3 +129,77 @@ class RepComprobanteIslr(models.AbstractModel):
         types = {'out_invoice': '1', 'in_invoice': '1', 'out_refund': '2',
                  'in_refund': '2'}
         return types[tipo]
+
+    def get_retenido_name(self, value):
+        res = ''
+        if 'out' in value.type:
+            res = value.partner_id.name
+        else:
+            res = value.company_id.partner_id.name
+        return res
+
+    def get_retenido_vat(self, value):
+        res = ''
+        if 'out' in value.type:
+            if value.partner_id.vat:
+                res = value.partner_id.vat[2:] if 'VE' in value.partner_id.vat else value.partner_id.vat
+        else:
+            if value.company_id.partner_id.vat:
+                res = value.company_id.partner_id.vat[2:] if 'VE' in value.company_id.partner_id.vat else value.company_id.partner_id.vat
+        return res
+
+    def get_retenido_phone(self, value):
+        res = ''
+        if 'out' in value.type:
+            if value.partner_id.vat:
+                res = value.partner_id.phone
+        else:
+            if value.company_id.partner_id.vat:
+                res = value.company_id.partner_id.phone
+        return res
+
+    #DATOS DEL AGENTE DE RETENIDO
+    def get_retencion_name(self, value):
+        res = ''
+        if 'out' in value.type:
+            res = value.company_id.partner_id.name
+        else:
+            res = value.partner_id.name
+        return res
+
+    def get_retencion_vat(self, value):
+        res = ''
+        if 'out' in value.type:
+            if value.company_id.partner_id.vat:
+                res = value.company_id.partner_id.vat[2:] if 'VE' in value.company_id.partner_id.vat else value.company_id.partner_id.vat
+        else:
+            if value.partner_id.vat:
+                res = value.partner_id.vat[2:] if 'VE' in value.partner_id.vat else value.partner_id.vat
+        return res
+
+    def get_retencion_dir(self, value):
+        direction = 'Sin direccion'
+        if 'out' in value.type:
+            direction = ((value.company_id.partner_id.street and value.company_id.partner_id.street + ', ') or '') + \
+                        ((value.company_id.partner_id.street2 and value.company_id.partner_id.street2 + ', ') or '') + \
+                        ((value.company_id.partner_id.city and value.company_id.partner_id.city + ', ') or '') + \
+                        ((
+                             value.company_id.partner_id.state_id.name and value.company_id.partner_id.state_id.name + ',') or '') + \
+                        ((
+                             value.company_id.partner_id.country_id.name and value.company_id.partner_id.country_id.name + '') or '')
+        else:
+            direction = ((value.partner_id.street and value.partner_id.street + ', ') or '') + \
+                        ((value.partner_id.street2 and value.partner_id.street2 + ', ') or '') + \
+                        ((value.partner_id.city and value.partner_id.city + ', ') or '') + \
+                        ((value.partner_id.state_id.name and value.partner_id.state_id.name + ',') or '') + \
+                        ((value.partner_id.country_id.name and value.partner_id.country_id.name + '') or '')
+
+        return direction
+
+    def get_retencion_phone(self, value):
+        res = ''
+        if 'out' in value.type:
+            res = value.company_id.partner_id.phone
+        else:
+            res = value.partner_id.phone
+        return res
--- l10n_ve_withholding_iva/report/withholding_vat.py
+++ l10n_ve_withholding_iva/report/withholding_vat.py
@@ -40,17 +40,17 @@ class IvaReport(models.AbstractModel):
         split_date = date.split('-')
         return str(split_date[2]) + '/' + (split_date[1]) + '/' + str(split_date[0])
 
-    def get_direction(self, partner):
-        direction = ''
-        direction = ((partner.street and partner.street + ', ') or '') +\
-                    ((partner.street2 and partner.street2 + ', ') or '') +\
-                    ((partner.city and partner.city + ', ') or '') +\
-                    ((partner.state_id.name and partner.state_id.name + ',')or '')+ \
-                    ((partner.country_id.name and partner.country_id.name + '') or '')
+    #def get_direction(self, partner):
+    #    direction = ''
+    #    direction = ((partner.street and partner.street + ', ') or '') +\
+    #                ((partner.street2 and partner.street2 + ', ') or '') +\
+    #                ((partner.city and partner.city + ', ') or '') +\
+    #                ((partner.state_id.name and partner.state_id.name + ',')or '')+ \
+    #                ((partner.country_id.name and partner.country_id.name + '') or '')
         #if direction == '':
         #    raise ValidationError ("Debe ingresar los datos de direccion en el proveedor")
             #direction = 'Sin direccion'
-        return direction
+    #    return direction
 
     def get_tipo_doc(self, tipo=None):
         if not tipo:
@@ -59,3 +59,100 @@ class IvaReport(models.AbstractModel):
                  'in_refund': '2'}
         return types[tipo]
 
+        # DATOS DEL AGENTE DE RETENCIÓN
+        # Direccion del agente retenido
+    def get_direction(self, value):
+            direction = 'Sin direccion'
+            if 'out' in value.type:
+                direction = ((value.partner_id.street and value.partner_id.street + ', ') or '') + \
+                            ((value.partner_id.street2 and value.partner_id.street2 + ', ') or '') + \
+                            ((value.partner_id.city and value.partner_id.city + ', ') or '') + \
+                            ((value.partner_id.state_id.name and value.partner_id.state_id.name + ',') or '') + \
+                            ((value.partner_id.country_id.name and value.partner_id.country_id.name + '') or '')
+            else:
+                direction = ((value.company_id.partner_id.street and value.company_id.partner_id.street + ', ') or '') + \
+                            ((
+                             value.company_id.partner_id.street2 and value.company_id.partner_id.street2 + ', ') or '') + \
+                            ((value.company_id.partner_id.city and value.company_id.partner_id.city + ', ') or '') + \
+                            ((
+                             value.company_id.partner_id.state_id.name and value.company_id.partner_id.state_id.name + ',') or '') + \
+                            ((
+                             value.company_id.partner_id.country_id.name and value.company_id.partner_id.country_id.name + '') or '')
+            return direction
+
+    def get_retenido_name(self, value):
+            res = ''
+            if 'out' in value.type:
+                res = value.partner_id.name
+            else:
+                res = value.company_id.partner_id.name
+            return res
+
+    def get_retenido_vat(self, value):
+            res = ''
+            if 'out' in value.type:
+                if value.partner_id.vat:
+                    res = value.partner_id.vat[2:] if 'VE' in value.partner_id.vat else value.partner_id.vat
+            else:
+                if value.company_id.partner_id.vat:
+                    res = value.company_id.partner_id.vat[
+                          2:] if 'VE' in value.company_id.partner_id.vat else value.company_id.partner_id.vat
+            return res
+
+    def get_retenido_phone(self, value):
+            res = ''
+            if 'out' in value.type:
+                if value.partner_id.vat:
+                    res = value.partner_id.phone
+            else:
+                if value.company_id.partner_id.vat:
+                    res = value.company_id.partner_id.phone
+            return res
+
+        # DATOS DEL AGENTE DE RETENIDO
+    def get_retencion_name(self, value):
+            res = ''
+            if 'out' in value.type:
+                res = value.company_id.partner_id.name
+            else:
+                res = value.partner_id.name
+            return res
+
+    def get_retencion_vat(self, value):
+            res = ''
+            if 'out' in value.type:
+                if value.company_id.partner_id.vat:
+                    res = value.company_id.partner_id.vat[
+                          2:] if 'VE' in value.company_id.partner_id.vat else value.company_id.partner_id.vat
+            else:
+                if value.partner_id.vat:
+                    res = value.partner_id.vat[2:] if 'VE' in value.partner_id.vat else value.partner_id.vat
+            return res
+
+    def get_retencion_dir(self, value):
+            direction = 'Sin direccion'
+            if 'out' in value.type:
+                direction = ((value.company_id.partner_id.street and value.company_id.partner_id.street + ', ') or '') + \
+                            ((
+                             value.company_id.partner_id.street2 and value.company_id.partner_id.street2 + ', ') or '') + \
+                            ((value.company_id.partner_id.city and value.company_id.partner_id.city + ', ') or '') + \
+                            ((
+                                 value.company_id.partner_id.state_id.name and value.company_id.partner_id.state_id.name + ',') or '') + \
+                            ((
+                                 value.company_id.partner_id.country_id.name and value.company_id.partner_id.country_id.name + '') or '')
+            else:
+                direction = ((value.partner_id.street and value.partner_id.street + ', ') or '') + \
+                            ((value.partner_id.street2 and value.partner_id.street2 + ', ') or '') + \
+                            ((value.partner_id.city and value.partner_id.city + ', ') or '') + \
+                            ((value.partner_id.state_id.name and value.partner_id.state_id.name + ',') or '') + \
+                            ((value.partner_id.country_id.name and value.partner_id.country_id.name + '') or '')
+
+            return direction
+
+    def get_retencion_phone(self, value):
+            res = ''
+            if 'out' in value.type:
+                res = value.company_id.partner_id.phone
+            else:
+                res = value.partner_id.phone
+            return res
