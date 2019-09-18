628ac1c4b3d05fca66a3c736a4f14b290c6ac133
MFLORES2018 <vasquezmiguelfa@gmail.com>
2019-09-18 12:48:08 -0400

    detalles que faltaban de liquidacion

--- tys_hr_payroll_payoff/model/hr_payroll_payoff.py
+++ tys_hr_payroll_payoff/model/hr_payroll_payoff.py
@@ -1,9 +1,11 @@
 # coding: utf-8
-from odoo import fields, models, api, exceptions
+from odoo import fields, models, api, exceptions, _
 from odoo import osv
 from datetime import datetime
 from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
 from dateutil.relativedelta import relativedelta
+from odoo.tools import float_compare, float_is_zero
+from odoo.exceptions import UserError
 
 
 class hr_payslip(models.Model):
@@ -64,6 +66,7 @@ class hr_payslip(models.Model):
     fecha_anticipo = fields.Date('Fecha del Anticipo')
     literal_c = fields.Float('Literal C')
     structure = fields.Many2one('hr.payroll.structure', 'Estructura', states={'draft': [('readonly', False)]})
+    move_id = fields.Many2one('account.move', 'Accounting Entry', readonly=True, copy=False)
 
     @api.onchange('employee_id')
     def onchange_employee_dos(self):
@@ -240,13 +243,104 @@ class hr_payslip(models.Model):
             # slip.write({'number': number}) #ya venia comentado!!!
             self = self.with_context({'struct_id': slip.struct_id.id, 'payoff': 'payoff',
                                       'payoff_date': datetime.now().strftime('%Y-%m-%d')})
-            #slip.action_payslip_done()
+            self.action_payslip_done_new()
             #self.compute_sheet_2()
             if slip.move_id:
                 slip.move_id.write(
                     {'name': '%s de %s' % (slip.struct_id.name, slip.employee_id.name)})
             self.employee_id.write({'active': False})
         return self.write({'state': 'done'})
+
+    @api.multi
+    def action_payslip_done_new(self):
+        precision = self.env['decimal.precision'].precision_get('Payroll')
+
+        for slip in self:
+            line_ids = []
+            debit_sum = 0.0
+            credit_sum = 0.0
+            date = slip.date or slip.date_to
+
+            name = _('Payslip of %s') % (slip.employee_id.name)
+            move_dict = {
+                'narration': name,
+                'ref': slip.number,
+                'journal_id': slip.journal_id.id,
+                'date': date,
+            }
+            for line in slip.details_by_salary_rule_category:
+                amount = slip.credit_note and -line.total or line.total
+                if float_is_zero(amount, precision_digits=precision):
+                    continue
+                debit_account_id = line.salary_rule_id.account_debit.id
+                credit_account_id = line.salary_rule_id.account_credit.id
+
+                if debit_account_id:
+                    debit_line = (0, 0, {
+                        'name': line.name,
+                        'partner_id': line._get_partner_id(credit_account=False),
+                        'account_id': debit_account_id,
+                        'journal_id': slip.journal_id.id,
+                        'date': date,
+                        'debit': amount > 0.0 and amount or 0.0,
+                        'credit': amount < 0.0 and -amount or 0.0,
+                        'analytic_account_id': line.salary_rule_id.analytic_account_id.id,
+                        'tax_line_id': line.salary_rule_id.account_tax_id.id,
+                    })
+                    line_ids.append(debit_line)
+                    debit_sum += debit_line[2]['debit'] - debit_line[2]['credit']
+
+                if credit_account_id:
+                    credit_line = (0, 0, {
+                        'name': line.name,
+                        'partner_id': line._get_partner_id(credit_account=True),
+                        'account_id': credit_account_id,
+                        'journal_id': slip.journal_id.id,
+                        'date': date,
+                        'debit': amount < 0.0 and -amount or 0.0,
+                        'credit': amount > 0.0 and amount or 0.0,
+                        'analytic_account_id': line.salary_rule_id.analytic_account_id.id,
+                        'tax_line_id': line.salary_rule_id.account_tax_id.id,
+                    })
+                    line_ids.append(credit_line)
+                    credit_sum += credit_line[2]['credit'] - credit_line[2]['debit']
+
+            if float_compare(credit_sum, debit_sum, precision_digits=precision) == -1:
+                acc_id = slip.journal_id.default_credit_account_id.id
+                if not acc_id:
+                    raise UserError(_('The Expense Journal "%s" has not properly configured the Credit Account!') % (
+                        slip.journal_id.name))
+                adjust_credit = (0, 0, {
+                    'name': _('Adjustment Entry'),
+                    'partner_id': False,
+                    'account_id': acc_id,
+                    'journal_id': slip.journal_id.id,
+                    'date': date,
+                    'debit': 0.0,
+                    'credit': debit_sum - credit_sum,
+                })
+                line_ids.append(adjust_credit)
+
+            elif float_compare(debit_sum, credit_sum, precision_digits=precision) == -1:
+                acc_id = slip.journal_id.default_debit_account_id.id
+                if not acc_id:
+                    raise UserError(_('The Expense Journal "%s" has not properly configured the Debit Account!') % (
+                        slip.journal_id.name))
+                adjust_debit = (0, 0, {
+                    'name': _('Adjustment Entry'),
+                    'partner_id': False,
+                    'account_id': acc_id,
+                    'journal_id': slip.journal_id.id,
+                    'date': date,
+                    'debit': credit_sum - debit_sum,
+                    'credit': 0.0,
+                })
+                line_ids.append(adjust_debit)
+            move_dict['line_ids'] = line_ids
+            move = self.env['account.move'].create(move_dict)
+            slip.write({'move_id': move.id, 'date': date})
+            move.post()
+        return
 
     @api.model
     def fields_view_get(self, view_id=None, view_type=False, toolbar=False, submenu=False):
@@ -327,14 +421,14 @@ class hr_payslip(models.Model):
                 vac_frac2 = (float(self.month_worked_year_str[0:1]) / float(12)) * float(pay_days)
                 self.vacaciones_fracc = vac_frac2
 
-        alic_b_v = self.calculo_alic_bono_vac((sal_mensual+(promedio*4)), dias_bv)
-        alic_u =  self.calculo_alic_util((sal_mensual+(promedio*4)),alic_b_v)
+        alic_b_v = self.calculo_alic_bono_vac((sal_mensual), dias_bv)
+        alic_u =  self.calculo_alic_util((sal_mensual),alic_b_v)
 
-        sal_diar = (((sal_mensual+(promedio*4))/2)/float(dias_str) + alic_b_v + alic_u)
+        sal_diar = (((sal_mensual)/2)/float(dias_str) + alic_b_v + alic_u)
         values.update({'salario_basico':sal_mensual,
                        'salario_basico_diario':sal_mensual/float(dias_str),
-                       'salario_prom_mensual': (sal_mensual + (promedio*4))/2,
-                       'salario_prom_diairo': ((sal_mensual + (promedio*4))/2) / float(dias_str),
+                  #     'salario_prom_mensual': (sal_mensual + (promedio*4))/2,
+                   #    'salario_prom_diairo': ((sal_mensual + (promedio*4))/2) / float(dias_str),
                        'alic_bono_vac_liq': alic_b_v,
                        'alic_util_liq': alic_u,
                        'salario_integral':sal_diar,
